7bb4c32e77715cd092f3fcb2096f769327adfa56
Unknown <boudon.eloise@gmail.com>
2017-11-06 10:50:30 +0100

    2 lignes description

--- partials/accueil.php
+++ partials/accueil.php
@@ -24,12 +24,12 @@ function afficher_liste_articles()
 
             <a href="index.php?page=2&id=<?php echo $donnees['id_article']; ?>&nom=<?php echo $donnees['titre']; ?>">
                 <div class="row">
-                    <div class="col-4">
+                    <div class="col-3">
                         <div class="article_image">
                             <img src="<?php echo $donnees['img_article']; ?>">
                         </div>
                     </div>
-                    <div class="col-8">
+                    <div class="col-9">
                         <div class="article_details">
                             <h2><?php echo htmlspecialchars($donnees['titre']); ?></h2>
                             <em> <?php echo $donnees['date_article']; ?></em>
