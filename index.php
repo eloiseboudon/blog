abee657a94089e0fc7d515b0bd581498f9c7a6bd
Unknown <boudon.eloise@gmail.com>
2017-11-06 17:58:31 +0100

    footer bien placé

--- index.php
+++ index.php
@@ -8,19 +8,20 @@
     <link href="https://fonts.googleapis.com/css?family=Dosis|Quicksand" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
+          integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 
     <link href="css/style.css" rel="stylesheet"/>
+    <link href="css/timeline.css" rel="stylesheet"/>
 </head>
 
 <body>
-    <div id="body-position">
-        <div class="barre_menu">
-            <?php include('partials/menu.php');?>
-        </div>
 
-        <div class="contenu">
-            <div class="global_width">
+<div class="page">
+        <?php include('partials/menu.php'); ?>
+
+    <div class="contenu">
+        <div class="global_width">
             <?php
             if (isset($_GET['page'])) {
                 switch ($_GET['page']) {
@@ -37,17 +38,24 @@
                 include('partials/accueil.php');
             }
             ?>
-            </div>
         </div>
-        <div id="footer">
-           <?php include ('partials/footer.php'); ?>
-        </div>
-
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</div>
+<div id="footer">
+    <?php include('partials/footer.php'); ?>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"
+        integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"
+        integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb"
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"
+        integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn"
+        crossorigin="anonymous"></script>
 
 </body>
 </html>
--- partials/accueil.php
+++ partials/accueil.php
@@ -17,72 +17,74 @@ function afficher_liste_articles()
 
 
     $req = $bdd->query($sql) or die ('Erreur SQL : ' . mysqli_error($bdd)); ?>
-    <div class="accueil_articles">
-        <?php while ($donnees = mysqli_fetch_array($req)) {
+    <section class="timeline">
+        <div class="accueil_articles">
+            <?php while ($donnees = mysqli_fetch_array($req)) {
             $nbLigne++;
             ?>
             <?php
             if (($nbLigne % 2) == 0) { ?>
-                <div class="accueil_articles_droite">
-                    <div class="etiquette_content etiquette_content_right right">
-                        <a href="index.php?page=2&id=<?php echo $donnees['id_article']; ?>&nom=<?php echo $donnees['titre']; ?>">
+            <div class="timeline-block timeline-block-right">
+                <div class="marker"></div>
+                <div class="scroll timeline-content">
+                    <a href="index.php?page=2&id=<?php echo $donnees['id_article']; ?>&nom=<?php echo $donnees['titre']; ?>">
 
-                            <div class="article_details">
-                                <h2><?php echo htmlspecialchars($donnees['titre']); ?></h2>
-                                <em> <?php echo $donnees['date_article']; ?></em>
+                        <div class="article_details">
+                            <h2><?php echo htmlspecialchars($donnees['titre']); ?></h2>
+                            <em> <?php echo $donnees['date_article']; ?></em>
 
-                                <div class="description_article">
-                                    <?php echo $donnees['description']; ?>
-                                </div>
-                                <div class="goto_article">
+                            <div class="description_article">
+                                <?php echo $donnees['description']; ?>
+                            </div>
+                            <div class="goto_article">
                                         <span><i class="fa fa-hand-o-right"
                                                  aria-hidden="true"></i> Voir l'article</span>
-                                </div>
                             </div>
+                        </div>
 
-                            <div class="article_image">
-                                <img src="<?php echo $donnees['img_article']; ?>">
-                            </div>
+                        <div class="article_image">
+                            <img src="<?php echo $donnees['img_article']; ?>">
+                        </div>
 
-                        </a>
-                    </div>
-                    <div class="etiquette_droite_haut"></div>
-                    <div class="etiquette_droite_bas"></div>
+                    </a>
                 </div>
+            </div>
 
-            <?php } else { ?>
-                <div class="accueil_articles_gauche">
-                    <div class="etiquette_content  etiquette_content_left left">
-                        <a href="index.php?page=2&id=<?php echo $donnees['id_article']; ?>&nom=<?php echo $donnees['titre']; ?>">
 
-                            <div class="article_image">
-                                <img src="<?php echo $donnees['img_article']; ?>">
+        <?php } else { ?>
+
+            <div class="timeline-block timeline-block-left">
+                <div class="marker"></div>
+                <div class="scroll timeline-content">
+                    <a href="index.php?page=2&id=<?php echo $donnees['id_article']; ?>&nom=<?php echo $donnees['titre']; ?>">
+
+                        <div class="article_image">
+                            <img src="<?php echo $donnees['img_article']; ?>">
+                        </div>
+
+                        <div class="article_details">
+                            <h2><?php echo htmlspecialchars($donnees['titre']); ?></h2>
+                            <em> <?php echo $donnees['date_article']; ?></em>
+
+                            <div class="description_article">
+                                <?php echo $donnees['description']; ?>
                             </div>
 
-                            <div class="article_details">
-                                <h2><?php echo htmlspecialchars($donnees['titre']); ?></h2>
-                                <em> <?php echo $donnees['date_article']; ?></em>
-
-                                <div class="description_article">
-                                    <?php echo $donnees['description']; ?>
-                                </div>
-
-                                <div class="goto_article">
+                            <div class="goto_article">
                                         <span><i class="fa fa-hand-o-right"
                                                  aria-hidden="true"></i> Voir l'article</span>
-                                </div>
                             </div>
+                        </div>
 
-                        </a>
-                    </div>
-                    <div class="etiquette_gauche_haut"></div>
-                    <div class="etiquette_gauche_bas"></div>
+                    </a>
                 </div>
-            <?php }
+            </div>
+        <?php }
 
 
         } ?>
-    </div>
+        </div>
+    </section>
     <?php
     mysqli_close($bdd);
 
