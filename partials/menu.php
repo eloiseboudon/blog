3e42d6f7d0cdddacbbcf3a94f76a852db99da61e
Unknown <boudon.eloise@gmail.com>
2017-11-07 11:17:38 +0100

    petite étiquette

--- partials/menu.php
+++ partials/menu.php
@@ -1,4 +1,5 @@
 <nav class="menu" id="menu">
     <div class="menu-item logo"><a href="/blog"><img src="assets/logo_entier.png"></a></div>
     <div class="menu-item"><a href="/blog">BLOG</a></div>
+    <div class="menu-item"><a href="/blog">BLOG</a></div>
 </nav>
