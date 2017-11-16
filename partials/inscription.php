4014078537a2d7ebcb11af71db133f788bdc0a54
Unknown <boudon.eloise@gmail.com>
2017-11-16 14:30:02 +0100

    inscription ok

--- partials/inscription.php
+++ partials/inscription.php
@@ -8,46 +8,57 @@
         <form action="sql/inscription.php" method="post">
 
             <label for="nom"><span>Nom <span class="required">*</span></span><input type="text" class="input-field"
-                                                                                    name="nom"/></label>
+                                                                                    name="nom" required="required"/></label>
             <label for="prenom"><span>Prenom <span class="required">*</span></span><input type="text"
                                                                                           class="input-field"
-                                                                                          name="prenom"/></label>
+                                                                                          name="prenom" required="required"/></label>
             <label for="pseudo"><span>Pseudo <span class="required">*</span></span><input type="text"
                                                                                           class="input-field"
-                                                                                          name="pseudo"/></label>
+                                                                                          name="pseudo" required="required"/></label>
             <label for="email"><span>Email <span class="required">*</span></span><input type="text" class="input-field"
-                                                                                        name="email"/></label>
+                                                                                        name="email" required="required"/></label>
             <label for="password"><span>Mot de passe <span class="required">*</span></span><input type="text"
                                                                                                   class="input-field"
-                                                                                                  name="password"/></label>
-            <p>
-                <label for="sexe"><span>Sexe<span class="required">*</span></span></label>
-                <input type="radio" name="sexe" value="femme" id="femme"/> Femme
-                <input type="radio" name="sexe" value="homme" id="homme"/> Homme
-                <input type="radio" name="sexe" value="neutre" id="neutre"/> Neutre
-            </p>
+                                                                                                  name="password" required="required"/></label>
+
+            <label for="sexe"><span>Sexe<span class="required">*</span></span>
+                <div class="input-sexe">
+                    <input type="radio" name="sexe" value="F" id="femme" required="required"/> Femme
+                    <input type="radio" name="sexe" value="H" id="homme" required="required"/> Homme
+                    <input type="radio" name="sexe" value="N" id="neutre" required="required"/> Neutre
+                </div>
+            </label>
+
             <label for="date_anniversaire"><span>Date de naissance (jj/mm/yyyy)<span
                             class="required">*</span></span><input type="text" class="input-field"
-                                                                   name="date_anniversaire"/></label>
+                                                                   name="date_anniversaire" required="required"/></label>
 
 
             <label for="adresse"><span>Adresse <span class="required">*</span></span><input type="text"
                                                                                             class="input-field"
-                                                                                            name="adresse"/></label>
+                                                                                            name="adresse" required="required"/></label>
             <label for="code_postal"><span>Code postal <span class="required">*</span></span><input type="text"
                                                                                                     class="input-field"
                                                                                                     name="code_postal"/></label>
 
             <label for="telephone"><span>Téléphone <span class="required">*</span></span><input type="text"
                                                                                                 class="input-field"
-                                                                                                name="telephone"/></label>
+                                                                                                name="telephone" required="required"/></label>
 
+            <label for="cgu">
+                <input type="checkbox" value="cgu" name="validate[]" required="required"/>J'ai lu et j'accepte les conditions générales d'utilisation<br/>
+            </label>
 
-            <input type="checkbox"/>J'ai lu et j'accepte les conditions générales d'utilisation<br/>
-            <input type="checkbox"/>J'accepte de recevoir des informations de la part de L'étiquette via courrier électronique<br />
-            <span class="required">(*) : Champs obligatoires</span>
+            <label for="courrier">
+                <input type="checkbox" value="courrier" name="validate[]"/>J'accepte de recevoir des informations de la part de L'étiquette via courrier
+                électronique<br/>
+            </label>
 
-          <input type="submit" value="Valider"/>
+            <div class="champs-obligatoires">
+                <span class="required">(*) : Champs obligatoires</span>
+            </div>
+
+            <input type="submit" value="Valider"/>
 
 
         </form>
--- sql/inscription.php
+++ sql/inscription.php
@@ -2,7 +2,6 @@
 include('connexion.php');
 
 if (isset($_POST['nom'])){
-    echo ($_POST['nom']);
 
     $bdd = connexion_sql();
 
@@ -10,13 +9,35 @@ if (isset($_POST['nom'])){
     $prenom = $_POST['prenom'];
     $pseudo = $_POST['pseudo'];
     $email = $_POST['email'];
-    $date = "12 sept";
+//    $date = "12 sept";
     $password= $_POST['password'];
+    $sexe= $_POST['sexe'];
+    $date_anniversaire = $_POST['date_anniversaire'];
+    $adresse = $_POST['adresse'];
+    $code_postal = $_POST['code_postal'];
+    $telephone = $_POST['telephone'];
+
+    $checkbox = array();
+    $checkbox= $_POST['validate'];
+//    $cgu = $checkbox[0];
+
+    if(count($checkbox)!= 1){
+        $courrier = 1;
+    }
+    else{
+        $courrier = 0;
+    }
+
+
+
+
 //    $pass_hache = sha1($_POST['password']);
 
-//    $sql = "INSERT INTO membres (nom, prenom, pseudo, password, email, date_inscription) VALUES ('$nom','$prenom','$pseudo','$password','$email', NOW())";
+    $sql = "INSERT INTO membres (nom, prenom, pseudo, password, email,sexe,date_anniversaire, adresse,code_postal,telephone,recevoir_mail,date_inscription) 
+VALUES ('$nom','$prenom','$pseudo','$password','$email','$sexe','$date_anniversaire','$adresse','$code_postal','$telephone','$courrier' ,NOW())";
 
-//    $req = $bdd->query($sql) or die ('Erreur SQL : ' . mysqli_error($bdd));
+
+    $req = $bdd->query($sql) or die ('Erreur SQL : ' . mysqli_error($bdd));
 
 }
 else{echo "non";}
