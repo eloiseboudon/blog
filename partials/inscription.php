972b2580231cf6e246dac5e0d33e5ca66710dbd2
Unknown <boudon.eloise@gmail.com>
2017-11-16 10:18:17 +0100

    design incription

--- partials/inscription.php
+++ partials/inscription.php
@@ -1,49 +1,54 @@
 <div class="inscription_compte">
     <h2>Inscription</h2>
     <div class="form-inscription">
-    <div class="cercle"></div>
-    <div class="ficelle"></div>
-
+        <div class="cercle"></div>
+        <div class="ficelle"></div>
 
 
         <form action="sql/inscription.php" method="post">
 
             <label for="nom"><span>Nom <span class="required">*</span></span><input type="text" class="input-field"
-                                                                                    name="nom"</label>
+                                                                                    name="nom"/></label>
             <label for="prenom"><span>Prenom <span class="required">*</span></span><input type="text"
                                                                                           class="input-field"
-                                                                                          name="prenom"</label>
+                                                                                          name="prenom"/></label>
             <label for="pseudo"><span>Pseudo <span class="required">*</span></span><input type="text"
                                                                                           class="input-field"
-                                                                                          name="pseudo"</label>
+                                                                                          name="pseudo"/></label>
             <label for="email"><span>Email <span class="required">*</span></span><input type="text" class="input-field"
-                                                                                        name="email"</label>
+                                                                                        name="email"/></label>
             <label for="password"><span>Mot de passe <span class="required">*</span></span><input type="text"
                                                                                                   class="input-field"
-                                                                                                  name="password"</label>
+                                                                                                  name="password"/></label>
             <p>
                 <label for="sexe"><span>Sexe<span class="required">*</span></span></label>
-                <input type="radio" name="sexe" value="femme" id="femme"/> Femme <br/>
-                <input type="radio" name="sexe" value="homme" id="homme"/> Homme <br/>
+                <input type="radio" name="sexe" value="femme" id="femme"/> Femme
+                <input type="radio" name="sexe" value="homme" id="homme"/> Homme
                 <input type="radio" name="sexe" value="neutre" id="neutre"/> Neutre
             </p>
             <label for="date_anniversaire"><span>Date de naissance (jj/mm/yyyy)<span
                             class="required">*</span></span><input type="text" class="input-field"
-                                                                   name="date_anniversaire"</label>
+                                                                   name="date_anniversaire"/></label>
 
 
             <label for="adresse"><span>Adresse <span class="required">*</span></span><input type="text"
-                                                                                                  class="input-field"
-                                                                                                  name="adresse"</label>
-            <label for="code_postal"><span>Code postal <span class="required">*</span></span><input type="text"
                                                                                             class="input-field"
-                                                                                            name="code_postal"</label>
+                                                                                            name="adresse"/></label>
+            <label for="code_postal"><span>Code postal <span class="required">*</span></span><input type="text"
+                                                                                                    class="input-field"
+                                                                                                    name="code_postal"/></label>
 
             <label for="telephone"><span>Téléphone <span class="required">*</span></span><input type="text"
-                                                                                            class="input-field"
-                                                                                            name="telephone"</label>
+                                                                                                class="input-field"
+                                                                                                name="telephone"/></label>
 
-            <label><span>&nbsp;</span><input type="submit" value="Submit"/></label>
+
+            <input type="checkbox"/>J'ai lu et j'accepte les conditions générales d'utilisation<br/>
+            <input type="checkbox"/>J'accepte de recevoir des informations de la part de L'étiquette via courrier électronique<br />
+            <span class="required">(*) : Champs obligatoires</span>
+
+          <input type="submit" value="Valider"/>
+
 
         </form>
     </div>
