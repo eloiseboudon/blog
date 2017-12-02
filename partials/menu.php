09e298ce8338b05bf00d14dd6c1013ed85a2079e
Unknown <boudon.eloise@gmail.com>
2017-12-02 08:46:38 +0100

    mentions légale responsive

--- partials/menu.php
+++ partials/menu.php
@@ -4,46 +4,45 @@
     <div id="acceuil" class="menu-item"><a href="/">Accueil</a></div>
     <div id="grand-ecran" class="menu-right">
         <?php
-        if (isset($_SESSION['user']['pseudo'])){
-            ?><div class="menu-item menu-item-right menu-pseudo">
-                <?php echo $_SESSION['user']['pseudo'] ?>
+        if ($_COOKIE['isConnect'] == 1) {
+            ?>
+            <div class="menu-item menu-item-right menu-pseudo">
+            <?php echo $_SESSION['user']['pseudo'] ?>
             </div>
             <div class="menu-item menu-item-right"><a href="sql/deconnexion.php"><i class="fa fa-user-times  "
-                                                                  aria-hidden="true"></i>
+                                                                                    aria-hidden="true"></i>
                     Déconnexion</a></div>
             <?php
-        }
-        else{
-        ?>
-        <div class="menu-item menu-item-right"><a href="index.php?page=4"><i class="fa fa-pencil"
-                                                                             aria-hidden="true"></i>
-                Inscription</a></div>
+        } else {
+            ?>
+            <div class="menu-item menu-item-right"><a href="index.php?page=4"><i class="fa fa-pencil"
+                                                                                 aria-hidden="true"></i>
+                    Inscription</a></div>
 
 
-        <div class="menu-item menu-item-right"><a href="index.php?page=3"><i class="fa fa-user"
-                                                                             aria-hidden="true"></i>
-                Connexion</a></div> <?php } ?>
+            <div class="menu-item menu-item-right"><a href="index.php?page=3"><i class="fa fa-user"
+                                                                                 aria-hidden="true"></i>
+                    Connexion</a></div> <?php } ?>
     </div>
 
     <div id="petit-ecran" class="menu-right">
         <?php
-        if (isset($_SESSION['user']['pseudo'])){
-        ?>
-        <div class="menu-item menu-item-right"><a href="sql/deconnexion.php"><i class="fa fa-user-times  "
-                                                                             aria-hidden="true"></i></a></div>
+        if ($_COOKIE['isConnect'] == 1) {
+            ?>
+            <div class="menu-item menu-item-right"><a href="sql/deconnexion.php"><i class="fa fa-user-times  "
+                                                                                    aria-hidden="true"></i></a></div>
 
-    <?php
-    }
-    else{
-    ?>
+            <?php
+        } else {
+            ?>
 
-        <div class="menu-item menu-item-right"><a href="index.php?page=4"><i class="fa fa-pencil"
-                                                                             aria-hidden="true"></i></a></div>
+            <div class="menu-item menu-item-right"><a href="index.php?page=4"><i class="fa fa-pencil"
+                                                                                 aria-hidden="true"></i></a></div>
 
 
-        <div class="menu-item menu-item-right"><a href="index.php?page=3"><i class="fa fa-user"
-                                                                             aria-hidden="true"></i></a></div>
-    <?php } ?>
+            <div class="menu-item menu-item-right"><a href="index.php?page=3"><i class="fa fa-user"
+                                                                                 aria-hidden="true"></i></a></div>
+        <?php } ?>
 
     </div>
 
